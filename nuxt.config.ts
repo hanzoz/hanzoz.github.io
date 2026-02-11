4e158538c37369eefda0cc624dfbcb9c7dcd39a5
Hanzo Chin <hanzoz@gmail.com>
2026-02-11 11:15:40 +0800

    add deployment to github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  compatibilityDate: '2025-12-31',
+  devtools: { enabled: true },
+  ssr: false,
+  app: {
+    baseURL: '/hanzoz.github.io/'
+  }
 })
