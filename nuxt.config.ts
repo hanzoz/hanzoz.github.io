4dc6299333599dafae90b80e90254661e9712ad3
Hanzo Chin <hanzoz@gmail.com>
2026-02-11 11:26:35 +0800

    add buildAssetsDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   app: {
-    baseURL: '/hanzoz.github.io/'
+    baseURL: '/hanzoz.github.io/',
+    buildAssetsDir: '/_nuxt/',
   }
 })
